ccd355514e7d3afc802fd9e95d1ee16657df2fb5
tavisca-rpawar <51902720+tavisca-rpawar@users.noreply.github.com>
2019-06-25 14:47:26 +0530

    Updated code
    Changes after review

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -21,52 +21,46 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         }
 
         public static int FindDigit(string equation)
-        {         
-            // Add your code here.
+        { // Add your code here.
             //Splitting The equation into three variables eg. 42*47=1?74 to A=42 B=47 and C=1?74
-            var A = equation.Split('*')[0];
-            var B = equation.Split('*')[1].Split('=')[0];
-            var C = equation.Split('=')[1];
+            string[] splitOnMultiply = equation.Split('*');
+            var A = splitOnMultiply[0];
+            string[] splitOnEqual = splitOnMultiply[1].Split('=');
+            var B = splitOnEqual[0];
+            var C = splitOnEqual[1];
             var result = -1;
             var ans = 0.0;
-            String originalValue = String.Empty, evaluatedValue;
+            string evaluatedEquation ="";
 
             //Checking which string variable contain "?" mark and storing the evaluated result in ans
+            if(C.Equals(0))
+                return result;
             if(A.Contains("?"))
             {
                 ans = double.Parse(C)/double.Parse(B);
-                originalValue = A;
+                evaluatedEquation= ans.ToString() + "*" + B + "=" + C; 
             }
             else if(B.Contains("?"))
             {
                 ans = double.Parse(C)/double.Parse(A);
-                originalValue = B;
+                evaluatedEquation = A + "*" + ans.ToString() + "=" + C;
             }
             else if(C.Contains("?"))
             {
                 ans = double.Parse(A) * double.Parse(B);
-                originalValue = C;
+                evaluatedEquation = A + "*" + B + "=" + ans.ToString();
             }
-            evaluatedValue = ans.ToString();
-
+            char tempChar;
             //Comparing the original and evaluated variables and finding the missing value
-            if(originalValue.Length == evaluatedValue.Length)
+            if(equation.Length == evaluatedEquation.Length)
             {
-                for(int i =0; i < evaluatedValue.Length; i++)
-                {
-                    if(originalValue[i] != evaluatedValue[i])
-                    {
-                        result = int.Parse(evaluatedValue[i].ToString());
-                        return  result;
-                    }
-
-                }
-            }
-            else 
-            {
+                int indexOfQuestionMark = equation.IndexOf('?');
+                tempChar = evaluatedEquation[ indexOfQuestionMark ];
+                result = tempChar  - '0';
                 return result;
             }
-            throw new NotImplementedException();
+            else 
+                return result; 
         }
     }
 }
